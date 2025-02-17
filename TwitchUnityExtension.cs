fe42da6597d9067afc470cd0afdad71a9ede7fe7
Loyalisti <retzkut@gmail.com>
2025-02-17 02:52:47 +0200

    Rename script files

--- TwitchUnityExtension.cs
+++ TwitchUnityExtension.cs
@@ -1,208 +1,146 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEditor;
-using UnityEditorInternal;
-using System.Collections.Generic;
-using TwitchClassDefinitions;
 using System.Net;
-using System.Numerics;
-using System.Threading.Tasks;
+using TwitchClassDefinitions;
 using System.IO;
+using UnityEngine.Rendering;
 using System;
-using System.Collections;
-using System.Security.Cryptography;
+using Unity.VisualScripting;
+using UnityEditor;
 
-public class TwitchUnityExtension : EditorWindow 
+public class Twitch_connection : MonoBehaviour
 {
-    private HttpListener listener; //redirect listener for authcode
+    // Start is called before the first frame update
 
-    private string appID;
-    private string appSecret;
-    private string authCode;
+    private string wsSessionID;
+    private string clientID;
+    private string clientSecret;
     private string authToken;
     private string refreshToken;
 
+    [SerializeField] string targetChannel;
 
-    private SerializedObject ExtensionObject;
-    private ReorderableList scopeReorderableList;
-    private ReorderableList eventReorderableList;
-    
-    [SerializeField] 
-    private List<string> scopeList = new List<string>(); // The actual list of strings
-    [SerializeField] 
-    private List<string> eventList = new List<string>(); // The actual list of strings
+    TwitchWebsocketInterface wsInterface; //main class used to read events
 
+    [SerializeField] private Boolean DebugWebSocket;
 
-    private string redirect_url = "http://localhost:2750/twitch-api/"; //This needs to be same as in dev.twitch application settings
-
-    [MenuItem("Window/Twitch Unity Extension")]
-    public static void ShowWindow()
+    void Start()
     {
-        GetWindow<TwitchUnityExtension>("Settings");
-    }
+        wsInterface = new TwitchWebsocketInterface();
+        if (DebugWebSocket)
+        { 
+            wsInterface.readyToConnect = true;
+            wsInterface.SetupWebsocketConnection(true);
+            return; 
+        } //Skip authentication part
 
-    private void OnEnable()
-    {
-        //Load default saved values
-        appID = EditorPrefs.GetString("TUE_AppID", null);
-        appSecret = EditorPrefs.GetString("TUE_AppSecret", null);
-        scopeList = new List<string>(EditorPrefs.GetString("TUE_Scopes", null).Split('+'));
-        eventList = new List<string>(EditorPrefs.GetString("TUE_Events", null).Split('+'));
-
-        ExtensionObject = new SerializedObject(this);
-        scopeReorderableList = new ReorderableList(ExtensionObject,ExtensionObject.FindProperty("scopeList") , true, false, true, true);
-        eventReorderableList = new ReorderableList(ExtensionObject,ExtensionObject.FindProperty("eventList") , true, false, true, true);
-
-        scopeReorderableList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
+        if (File.Exists(Application.persistentDataPath+"extension-token"))
         {
-            if(scopeList.Count > 0)
-            {
-                scopeList[index] = EditorGUI.TextField(rect,scopeList[index]);
-            }
-        };
-        scopeReorderableList.onAddCallback = (rect) =>
-        {
-            scopeList.Add("");
-        };
-        scopeReorderableList.onRemoveCallback = (ReorderableList list) =>
-        {
-            if(list.index >= 0)
-            {
-                scopeList.RemoveAt(list.index);
-                ExtensionObject.ApplyModifiedProperties();
-            }
-        };
+            refreshToken = File.ReadAllText(Application.persistentDataPath + "extension-token");
+            Debug.Log("Refresh token received");
+            
+            clientID = EditorPrefs.GetString("TUE_AppID", null);
+            clientSecret = EditorPrefs.GetString("TUE_AppSecret", null);
 
-        eventReorderableList.drawElementCallback = (Rect rect, int index, bool isActive, bool isFocused) =>
-        {
-            if(eventList.Count > 0)
-            {
-                eventList[index] = EditorGUI.TextField(rect,eventList[index]);
-            }
-        };
-        eventReorderableList.onAddCallback = (rect) =>
-        {
-            eventList.Add("");
-        };
-        eventReorderableList.onRemoveCallback = (ReorderableList list) =>
-        {
-            if(list.index >= 0)
-            {
-                eventList.RemoveAt(list.index);
-                ExtensionObject.ApplyModifiedProperties();
-            }
-        };
-    }
-
-    private void OnGUI()
-    {
-        GUILayout.Label("Twitch Application Keys", EditorStyles.boldLabel);
-
-        appID = EditorGUILayout.TextField("Application ID",appID);
-        appSecret = EditorGUILayout.TextField("Application Secret",appSecret);
-
-        GUILayout.Label("Scopes", EditorStyles.boldLabel);
-        scopeReorderableList.DoLayoutList();
-
-
-        GUILayout.Label("Events", EditorStyles.boldLabel);
-
-        eventReorderableList.DoLayoutList();
-        ExtensionObject.Update();
-        ExtensionObject.ApplyModifiedProperties();
-
-        if (GUILayout.Button("Authenticate"))
-        {
-            //Open browser with correct authorization url + parameters
-            Application.OpenURL($"https://id.twitch.tv/oauth2/authorize?response_type=code&client_id={appID}&redirect_uri=http://localhost:2750/twitch-api/&scope={string.Join("+",scopeList)}&force_verify=true");
-            AuthenticateApplication(); //will start seperate thread to get the token and authenticate. Refresh token will be stored in file.
+            StartCoroutine(RefreshAuthenticationToken());
+            wsInterface.readyToConnect = true;
+            wsInterface.SetupWebsocketConnection();
+            return;
         }
 
-        if (GUILayout.Button("Save Settings"))
-        {
-            EditorPrefs.SetString("TUE_AppID", appID);
-            EditorPrefs.SetString("TUE_AppSecret", appSecret);
-            EditorPrefs.SetString("TUE_Scopes", string.Join("+", scopeList));
-            EditorPrefs.SetString("TUE_Events", string.Join("+", eventList));
-        }
-
-        if (GUILayout.Button("Load Settings"))
-        {
-            appID = EditorPrefs.GetString("TUE_AppID", null);
-            appSecret = EditorPrefs.GetString("TUE_AppSecret", null);
-            scopeList = new List<string>(EditorPrefs.GetString("TUE_Scopes", null).Split('+'));
-            eventList = new List<string>(EditorPrefs.GetString("TUE_Events", null).Split('+'));
-
-            ExtensionObject.Update();
-            ExtensionObject.ApplyModifiedProperties();
-        }
+        Debug.Log("Extension is not authenticated. Go to setting to reauthenticate");
     }
 
-    private void AuthenticateApplication()
+    void OnDestroy()
     {
-        startRedirectListener(); //used to listen for redirect into the redirect url;
-        AuthorizeApplicationCode();
+        wsInterface.DisconnectFromWebSocket();
     }
 
-    private void startRedirectListener()
+    private void Update()
     {
-        listener = new HttpListener();
-        listener.Prefixes.Add(redirect_url);
-        listener.Start();
-        Debug.Log("listener started");
-
-        //listener.TimeoutManager.EntityBody.Add(TimeSpan.FromSeconds(10));
-
-        while (listener.IsListening) {
-            IAsyncResult result = listener.BeginGetContext(redirectListenerCallback, null);
-            result.AsyncWaitHandle.WaitOne(1000);
-            Debug.Log("Listener is waiting");
-        }
+        wsInterface.Update(); //just overwrite for dispatch message
     }
 
-    private void redirectListenerCallback(System.IAsyncResult result)
+    //this is kinda finicy. Sometimes if old authentication site is left open this will trigger and cause invalid token to be created.
+    //Please make sure there arent such windows open.
+    IEnumerator RefreshAuthenticationToken()
     {
-        HttpListenerContext context = listener.EndGetContext(result);
-        HttpListenerRequest request = context.Request;
-        string url = request.Url.ToString();
-
-        if (url.Contains("code"))
-        { //Redirect found
-            Debug.Log("authCode received");
-            authCode = url.Split('=')[1].Split('&')[0]; //this contains the authorization code that is used to get auth token later.
-            listener.Stop();
-        }
-    } 
-
-    private void AuthorizeApplicationCode()
-    {
-        Debug.Log("Entered Authorization");
-        using (var request = new UnityWebRequest($"https://id.twitch.tv/oauth2/token?client_id={appID}&redirect_uri={redirect_url}&code={authCode}&grant_type=authorization_code&client_secret={appSecret}", "POST"))
+        while (!wsInterface.readyToConnect)
         {
-            request.timeout = 10;
-            request.downloadHandler = new DownloadHandlerBuffer();
-            request.SendWebRequest();
-            Debug.Log("Request sent");
-            while (!request.isDone) { } //Hang here to wait for response
-            if (request.result == UnityWebRequest.Result.Success)
+            yield return new WaitForSeconds(.5f);
+        }
+        var request = new UnityWebRequest($"https://id.twitch.tv/oauth2/token?client_id={clientID}&refresh_token={refreshToken}&grant_type=refresh_token&client_secret={clientSecret}", "POST");
+        request.downloadHandler = new DownloadHandlerBuffer();
+        yield return request.SendWebRequest();
+        refreshtokenresponse response = JsonUtility.FromJson<refreshtokenresponse>(request.downloadHandler.text);
+        if (response.access_token != null)
+        {
+            authToken = response.access_token;
+            refreshToken = response.refresh_token;
+            SaveRefreshToken();
+            Debug.Log($"New Refresh token is: {refreshToken}");
+            Debug.Log("authToken received");
+            StartCoroutine(SubscribeToInitialEvent());
+            string[] eventList = EditorPrefs.GetString("TUE_Events", null).Split('+'); //Events from the setting page
+            if (eventList[0] == "") { Debug.Log("No events defined in config"); yield break; }
+            foreach (string event_name in eventList)
             {
-                authcoderesponse response = JsonUtility.FromJson<authcoderesponse>(request.downloadHandler.text);
-                Debug.Log("Response received");
-                if (response.access_token != null)
-                {
-                    authToken = response.access_token;
-                    refreshToken = response.refresh_token;
-                    Debug.Log($"Authentication Token {response.refresh_token}");
-                    SaveRefreshToken();
-                }
-            }
-            else
-            {
-                Debug.Log("error occured");
+                StartCoroutine(SubscribeToEvent(event_name.Split(",")[0], event_name.Split(",")[1]));
             }
         }
+        else
+        {
+            Debug.Log("Failed to receive authToken");
+        }
+        yield return null;
     }
 
+    IEnumerator SubscribeToInitialEvent()
+    {
+        RequestObject requestChat = new RequestObject();
+        requestChat.condition = new ConditionObject();
+        requestChat.transport = new TransportObject();
+        requestChat.type = "channel.chat.message";
+        requestChat.version = "1";
+        requestChat.condition.broadcaster_user_id = targetChannel;
+        requestChat.condition.user_id = targetChannel;
+        requestChat.transport.method = "websocket";
+        requestChat.transport.session_id = wsInterface.wsSession;
+
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(JsonUtility.ToJson(requestChat));
+        var request = new UnityWebRequest("https://api.twitch.tv/helix/eventsub/subscriptions", "POST");
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Client-ID", clientID);
+        request.SetRequestHeader("Authorization", "Bearer " + authToken);
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+        if (request.error != null) { Debug.Log("Subscription error response: " + request.error.ToString()); }
+        else { Debug.Log($"Subscription to event: channel.chat.message was successful"); }
+    }
+    IEnumerator SubscribeToEvent(string event_name, string version)
+    {
+        RequestObject requestChat = new RequestObject();
+        requestChat.condition = new ConditionObject();
+        requestChat.transport = new TransportObject();
+        requestChat.type = event_name;
+        requestChat.version = version;
+        requestChat.condition.broadcaster_user_id = targetChannel;
+        requestChat.transport.method = "websocket";
+        requestChat.transport.session_id = wsInterface.wsSession;
+
+        byte[] bodyRaw = System.Text.Encoding.UTF8.GetBytes(JsonUtility.ToJson(requestChat));
+        var request = new UnityWebRequest("https://api.twitch.tv/helix/eventsub/subscriptions", "POST");
+        request.uploadHandler = (UploadHandler)new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Client-ID", clientID);
+        request.SetRequestHeader("Authorization", "Bearer " + authToken);
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+        if (request.error != null) { Debug.Log("Subscription error response: " + request.error.ToString()); }
+        else { Debug.Log($"Subscription to event: {event_name} was successful"); }
+    }
     void SaveRefreshToken()
     {
         //TODO
@@ -210,3 +148,4 @@ public class TwitchUnityExtension : EditorWindow
         Debug.Log("Refresh token written to secure txt file");
     }
 }
+
